9a212e9974c9c78de5611bd674f5777aa7727b26
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-02-09 03:50:25 +0000

    main file

--- app/database/db.js
+++ app/database/db.js
@@ -1,37 +1,10 @@
 import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
+import conn from './conn';
 import _ from 'lodash';
 import Faker from 'faker';
 
-dotenv.config();
 
-const dataBase = process.env.DATABASE;
-const userName = process.env.USERNAME;
-const passWord = process.env.PASSWORD;
-console.log(dataBase, userName, passWord);
 
-const conn = new Sequelize(
-    dataBase, 
-    'root', 
-    passWord,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    })
-
-conn.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 const Person = conn.define('person', {
     firstName: {
